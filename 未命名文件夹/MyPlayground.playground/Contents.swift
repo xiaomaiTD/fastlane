a08ac3af5bc4f0699779a1c46bf3e350e91b6da1
xiaomaiTD <291104471@qq.com>
2018-04-01 09:07:06 +0800

    add

---
+++ 未命名文件夹/MyPlayground.playground/Contents.swift
@@ -0,0 +1,115 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+//
+//struct ValueType {
+//    var name:String
+//}
+//
+//var object = ValueType(name: "zhuo")
+//let name = \ValueType.name
+//// set
+//object[keyPath: name] = "swift4"
+//// get
+//let valueOfName = object[keyPath:name]
+//
+//var object1 = ValueType(name:"zhu")
+//let name1 = \ValueType.name
+//
+//
+//var optionalInteger: Int?
+//
+//
+//optionalInteger
+//
+//var myString:String? = nil
+//
+//if myString != nil {
+//    print(myString)
+//}else{
+//    print("字符串为 nil")
+//}
+//
+//struct Kid {
+//    var nickname: String = ""
+//    var age: Double = 0.0
+//    var friends: [Kid] = []
+//}
+//
+//var ben = Kid(nickname: "Benji", age: 8, friends: [])
+//
+//let name3  = ben[keyPath: \Kid.nickname]
+//
+//let name2 = ben[keyPath: \Kid.nickname]
+//ben[keyPath: \Kid.nickname] = "BigBen"
+//
+//let name4  = ben[keyPath: \Kid.nickname]
+//
+//let values = "one,two,three..."
+//var i = values.startIndex
+//
+//while let comma = values[i..<values.endIndex].index(of: ",") {
+//    print(comma)
+//    if values[i..<comma] == "two" {
+//        print("found it!")
+//    }
+//    i = values.index(after: comma)
+//}
+//
+//let values1 = "abcdefghijklmnopqrst"
+//let startSlicingIndex = values1.index(values.startIndex, offsetBy: 3)
+//let subvalues = values1[startSlicingIndex...]
+//
+//let abc: String = "abc"
+//_ = abc.map {
+//    print($0)
+//}
+//
+//// filter
+//let filtered = abc.filter { $0 == "b" }
+//
+//// reduce
+//let result = abc.reduce("1") { (result, c) -> String in
+//    print(result)
+//    print(c)
+//    return result + String(c)
+//}
+//print(result)
+//
+//
+//var myString1:String?
+//myString1 = "Hello, Swift!"
+//
+//if let yourString = myString1 {
+//    print("你的字符串值为 - \(yourString)")
+//}else{
+//    print("你的字符串没有值")
+//}
+//
+//let webName = "百度"
+//let webSite = "www.baidu.com"
+//
+//print("\(webName)的官网是:"+webSite)
+
+//for index in 1...5 {
+//    print("\(index) * 5 = \(index * 5)")
+//}
+
+for index in 1..<5 {
+    print("\(index) * 5 = \(index * 5)")
+}
+
+
+var str = "123";
+var str1 = "123";
+
+
+
+
+
+
+
+
+
